b839e2340a32fde8df02d5a7037960b497010e3d
Avextro <leightoncook45@gmail.com>
2026-07-05 19:38:52 +1000

    EMU | feat: init SDL2 video subsystem

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int SDL_main(int argc, char *argv[])
 {
     std::cout << "Vexel Display Emulator Starting..." << std::endl;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    std::cout << "SDL Initialized Successfully!" << std::endl;
+
+    SDL_Quit();
+
     return 0;
 }
