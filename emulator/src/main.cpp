210122d64499ed19074fc520e9a9172d06652eff
Avextro <leightoncook45@gmail.com>
2026-07-05 19:41:41 +1000

    EMU | feat: create SDL window & renderer

--- emulator/src/main.cpp
+++ emulator/src/main.cpp
@@ -11,6 +11,23 @@ int SDL_main(int argc, char *argv[])
         return -1;
     }
 
+    SDL_Window *window = SDL_CreateWindow("Vexel Display Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        std::cerr << "Failed to create SDL renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
     std::cout << "SDL Initialized Successfully!" << std::endl;
 
     SDL_Quit();
