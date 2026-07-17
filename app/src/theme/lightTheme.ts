4f75a348939f4a70546b0f818c2bdc4b447f14c6
Avextro <leightoncook45@gmail.com>
2026-07-18 01:08:49 +1000

    feat: create spacing, typography, lighttheme

---
+++ app/src/theme/lightTheme.ts
@@ -0,0 +1,9 @@
+import {colours} from "./colours"
+import {spacing} from "./spacing"
+import {typography} from "./typography"
+
+export const lightTheme = {
+    colours,
+    spacing,
+    typography,
+}
---
+++ app/src/theme/typography.ts
@@ -0,0 +1,8 @@
+export const typography = {
+    title: 24, 
+    heading: 20,
+    bodyLarge: 16,
+    body: 14,
+    bodySmall: 12,
+    caption: 10,
+}
