1f4c4b71e77854ac5db23c6faf3bb6397ebc7a1e
Avextro <leightoncook45@gmail.com>
2026-07-18 01:20:34 +1000

    feat: add sizes

--- app/src/theme/lightTheme.ts
+++ app/src/theme/lightTheme.ts
@@ -1,9 +1,11 @@
 import {colours} from "./colours"
 import {spacing} from "./spacing"
 import {typography} from "./typography"
+import { sizes } from "./sizes"
 
 export const lightTheme = {
     colours,
     spacing,
     typography,
-}
+    sizes
+};
---
+++ app/src/theme/sizes.ts
@@ -0,0 +1,12 @@
+export const sizes = {
+  iconSizeSmall: 16,
+  iconSize: 24,
+  iconSizeLarge: 32,
+
+  buttonHeightSmall: 36,
+  buttonHeight: 46,
+  buttonHeightLarge: 56,
+
+  tabBarHeight: 64,
+  headerHeight: 56,
+};
