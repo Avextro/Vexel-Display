ba3944485a6343dad773f3910f6e25a696649fdc
Avextro <leightoncook45@gmail.com>
2026-07-18 00:56:44 +1000

    feat: create colour theme

---
+++ app/src/theme/colours.ts
@@ -0,0 +1,37 @@
+export const colours = {
+            primary: "#00696F",
+            onPrimary: "#FFFFFF",
+            primaryContainer: "#45BCC5",
+            onPrimaryContainer: "#00484C",
+
+            secondary: "#875200",
+            onSecondary: "#FFFFFF",
+            secondaryContainer: "#E39D48",
+            onSecondaryContainer: "#5D3700",
+
+            error: "#BA1A1A",
+            onError: "#FFFFFF",
+            errorContainer: "#FFDAD6",
+            onErrorContainer: "#93000A",
+
+            background: "#F5FAFA",
+            onBackground: "#171D1D",
+
+            surface: "#FAF9F9",
+            onSurface: "#1A1C1C",
+            surfaceVariant: "#D8E5E6",
+            onSurfaceVariant: "#3D494A",
+
+            outline: "#6D797A",
+            outlineVariant: "#BCC9CA",
+
+            shadow: "#000000",
+
+            surfaceDim: "#DADADA",
+            surfaceBright: "#FAF9F9",
+            surfaceContainerLowest: "#FFFFFF",
+            surfaceContainerLow: "#F4F3F3",
+            surfaceContainer: "#EEEEED",
+            surfaceContainerHigh: "#E8E8E8",
+            surfaceContainerHighest: "#E2E2E2"
+}
